c995675b789accdb55524ea7b26f068bf6e80dd1
Andrew Vaughan <lilmenso101@gmail.com>
2015-01-23 13:19:33 -0800

    Completed login page animation and styling

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  $('.checker').hide();
+  $('.tagline').hide();
+  $('.action-maker').hover(function(){
+    $('.checker').show();
+    $('.tagline').show();
+    $('html').css('background-image', 'none');
+    $('html').css('background-color', 'black');
+    $('#maybeforms').hide();
+    $('.spacer').hide();
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  })
 });
+
+
